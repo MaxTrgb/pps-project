5eb16436c51741728d1b568bfb19d04919fe2ce8
MaxTrgb <maksym.tryhub@gmail.com>
2024-04-04 19:04:03 +0300

    finished main page

--- red-gulp-template/src/js/script.js
+++ red-gulp-template/src/js/script.js
@@ -1,42 +1,31 @@
-const wiperTrack = document.querySelector(".wiper-track");
-const wipes = Array.from(wiperTrack.children);
-const wipePrevBtn = document.querySelector(".wiper-button__right");
-const wipeNextBtn = document.querySelector(".wiper-button__left");
-const wipeWidth = wipes[0].getBoundingClientRect().width;
+document.addEventListener("DOMContentLoaded", function () {
+  const restaurantContainer = document.querySelector(".card-slider");
+  const leftRButton = document.querySelector(".restaurant-arrow-left");
+  const rightRButton = document.querySelector(".restaurant-arrow-right");
 
-const arrowsBehaviour = (wipePrevBtn, wipeNextBtn, index) => {
-  if (index === 0) {
-    wipePrevBtn.classList.add("is-hidden");
-    wipeNextBtn.classList.remove("is-hidden");
-  } else if (index === wipes.length-1) {
-    wipePrevBtn.classList.remove("is-hidden");
-    wipeNextBtn.classList.add("is-hidden");
-  } else {
-    wipePrevBtn.classList.remove("is-hidden");
-    wipeNextBtn.classList.remove("is-hidden");
+  function updateButtonState() {
+    leftRButton.disabled = restaurantContainer.scrollLeft <= 0;
+    rightRButton.disabled =
+      restaurantContainer.scrollLeft + restaurantContainer.offsetWidth >=
+      restaurantContainer.scrollWidth;
   }
-};
 
-const wipeSlide = (wiperTrack, activeSlide, nextSlide, targetIndex) => {
-  wiperTrack.style.transform =
-    "translateX(-" + (wipeWidth + 24) * (targetIndex - 1) + "px)";
-  activeSlide.classList.remove("active-swipe");
-  activeSlide.style.transform = "scale(1)";
-  nextSlide.classList.add("active-swipe");
-  nextSlide.style.transform = "scale(1.1)";
-};
+  leftRButton.onclick = function () {
+    restaurantContainer.scrollBy({
+      left: -restaurantContainer.offsetWidth / 2,
+      behavior: "smooth",
+    });
+  };
 
-wipeNextBtn.addEventListener("click", (e) => {
-  const activeSlide = wiperTrack.querySelector(".active-swipe");
-  const nextSlide = activeSlide.nextElementSibling;
-  const targetIndex = wipes.findIndex((slide) => slide === nextSlide);
-  wipeSlide(wiperTrack, activeSlide, nextSlide, targetIndex);
-  arrowsBehaviour(wipePrevBtn, wipeNextBtn, targetIndex);
+  rightRButton.onclick = function () {
+    restaurantContainer.scrollBy({
+      left: restaurantContainer.offsetWidth / 2,
+      behavior: "smooth",
+    });
+  };
+
+  restaurantContainer.addEventListener("scroll", updateButtonState);
+  updateButtonState();
 });
-wipePrevBtn.addEventListener("click", (e) => {
-  const activeSlide = wiperTrack.querySelector(".active-swipe");
-  const nextSlide = activeSlide.previousElementSibling;
-  const targetIndex = wipes.findIndex((slide) => slide === nextSlide);
-  wipeSlide(wiperTrack, activeSlide, nextSlide, targetIndex);
-  arrowsBehaviour(wipePrevBtn, wipeNextBtn, targetIndex);
-});
+
+  
